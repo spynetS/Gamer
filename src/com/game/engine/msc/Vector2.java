398cc01802836bf0906f515789091745a686bb35
= <alfred@stensatter.se>
2023-11-17 10:55:08 +0100

    changed vector.down/up to be correct

--- src/com/game/engine/msc/Vector2.java
+++ src/com/game/engine/msc/Vector2.java
@@ -4,8 +4,8 @@ import java.util.Objects;
 
 public class Vector2 {
 
-    public static Vector2 up = new Vector2(0,-1);
-    public static Vector2 down = new Vector2(0,1);
+    public static Vector2 up = new Vector2(0,1);
+    public static Vector2 down = new Vector2(0,-1);
     public static Vector2 right = new Vector2(1,0);
     public static Vector2 left = new Vector2(-1,0);
     public static Vector2 zero = new Vector2(0,0);
