4ff0d24576b224ccc31b05bec66d201d2cddc166
Alfred <alfred@stensatter.se>
2025-01-19 21:57:14 +0100

    removed some more lombok stuff

--- src/com/game/engine/test/Main.java
+++ src/com/game/engine/test/Main.java
@@ -3,11 +3,14 @@ package com.game.engine.test;
 import com.game.engine.GameEngine;
 import com.game.engine.GameObject;
 import com.game.engine.Input.Input;
+import com.game.engine.collision.Collider;
 import com.game.engine.Scene;
 import com.game.engine.components.*;
+import com.game.engine.components.Component;
 import com.game.engine.msc.Debug;
 import com.game.engine.msc.Vector2;
 import com.game.engine.physics.Rigidbody;
+import com.game.engine.rendering.Renderer;
 import com.game.engine.rendering.ShapeRender;
 import com.game.engine.rendering.Sprite;
 
@@ -28,11 +31,24 @@ public class Main {
                 @Override
                 public void update(){
                     super.update();
-                    this.transform.translate(new Vector2(1,0));
+                    //this.transform.translate(new Vector2(1,0));
                 }
             };
         player.addComponent(new ShapeRender());
+        player.addComponent(new PlayerMovement());
+        player.addComponent(new Rigidbody(true));
+        player.addComponent(new Collider());
         scene.add(player);
+
+
+        RectangleGameObject floor = new RectangleGameObject();
+        floor.getComponent(ShapeRender.class).setColor(Color.RED);
+        floor.transform.setScale(new Vector2(500,100));
+        floor.transform.setPosition(new Vector2(0,400));
+        floor.addComponent(new Rigidbody(false));
+        floor.addComponent(new Collider());
+        scene.add(floor);
+
 
         GameEngine.setSelectedScene(scene);
         gameEngine.start();
